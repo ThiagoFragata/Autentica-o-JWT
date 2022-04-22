a943547135975e54f267e031421c3c0769ee6743
Thiago Henrique Fragata <thiago Henrique Fragata>
2022-04-22 17:29:29 -0400

    Hooks para lidar com permissões de usuarios

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,15 +1,22 @@
 import { useContext } from "react";
 import { AuthContext } from "../contexts/AuthContext";
+import { useCan } from "../hooks/useCan";
 import { setupAPIClient } from "../services/api";
 import { WithSSRAuth } from "../utils/withSSRAuth";
 
 export default function Dashboard() {
   const { user } = useContext(AuthContext);
 
+  const userCanSeeMetrics = useCan({
+    roles: ["administrator", "editor"],
+  });
+
   return (
     <div>
       <h1>Dashboard</h1>
       <p>{user?.email}</p>
+
+      {userCanSeeMetrics && <div>Métricas</div>}
     </div>
   );
 }
